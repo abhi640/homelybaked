808b2b4883098d43dfe5093a253ca6f3ab36a3b6
abhi640 <abhi6340@gmail.com>
2022-01-29 13:17:26 +0530

    font family corrected

--- src/components/hero/hero.js
+++ src/components/hero/hero.js
@@ -1,5 +1,11 @@
 import cookiepic from "../../images/cookies3.webp";
 import "./hero.css";
+import customer1 from "../../images/customers/customer-1.jpg";
+import customer2 from "../../images/customers/customer-2.jpg";
+import customer3 from "../../images/customers/customer-3.jpg";
+import customer4 from "../../images/customers/customer-4.jpg";
+import customer5 from "../../images/customers/customer-5.jpg";
+import customer6 from "../../images/customers/customer-5.jpg";
 
 const Hero = () => (
   <section className="section-hero">
@@ -15,6 +21,19 @@ const Hero = () => (
         <a href="#" className="btn">
           Order Now
         </a>
+        <div class="delivered-meals">
+          <div class="delivered-imgs">
+            <img src={customer1} alt="Customer photo" />
+            <img src={customer2} alt="Customer photo" />
+            <img src={customer3} alt="Customer photo" />
+            <img src={customer4} alt="Customer photo" />
+            <img src={customer5} alt="Customer photo" />
+            <img src={customer6} alt="Customer photo" />
+          </div>
+          <p class="delivered-text">
+            <span>250,000+</span> meals delivered last year!
+          </p>
+        </div>
       </div>
       <div className="hero-img-box">
         <img className="hero-img" src={cookiepic} />
